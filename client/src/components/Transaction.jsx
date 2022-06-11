3f9bbbad2344b8b3145398574c6f5f5ad7a2127b
joe061005 <18233368@life.hkbu.edu.hk>
2022-06-12 01:36:12 +0800

    edit the transaction component

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -5,6 +5,47 @@ import { TransactionContext } from '../context/TransactionContext'
 import dummyData from '../util/dummyData'
 import { shortenAddress } from '../util/shortenAddress'
 
+const TransactionCard = ({ addressTo, addressFrom, amount, timestamp, message, keyword }) => {
+  return (
+    <div className="bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-col p-3 rounded-md hover:shadow-2xl
+    ">
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="w-full mb-6 p-2">
+          <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener moreferrer">
+            <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+          </a>
+          <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener moreferrer">
+            <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+          </a>
+          <p className="text-white text-base">Amount: {amount}ETH</p>
+          {
+            message && (
+              <>
+                 <br/>
+                 <p className="text-white text-base">Message: {message}</p>
+              
+              </>
+            )
+          }
+
+          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+            <p className="text-[#37c7da] font-bold"></p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
 const Transaction = () => {
 
   const { currentAccount } = useContext(TransactionContext)
@@ -27,7 +68,7 @@ const Transaction = () => {
 
         <div className="flex flex-wrap justify-center items-center mt-10">
           {dummyData.reverse().map((transaction, i) => (
-            <TransactionCard key={i} {...transaction}/>
+            <TransactionCard key={i} {...transaction} />
           ))}
         </div>
 
